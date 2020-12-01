9e97df4ee4759703d037bf1c38dcda0e9dccf075
Ed Tshuma <edtshuma@outlook.com>
2020-12-01 17:11:36 +0200

    Register Employee service  with DI container

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace EmployeeAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<EmployeeContext>(o => o.UseSqlServer(Configuration.GetConnectionString("EmployeeDB")));
-           
+            services.AddTransient<IEmployeeService, EmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
