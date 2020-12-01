ba338a87120476c102122d5bf849b960165087cf
Ed Tshuma <edtshuma@outlook.com>
2020-12-01 17:57:58 +0200

    Enable EF Core migrations

---
+++ Migrations/20201201155655_InitialCreate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Sex = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    EmploymentTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmploymentTypes",
+                columns: table => new
+                {
+                    EmploymentTypeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmploymentTypes", x => x.EmploymentTypeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "EmploymentTypes",
+                columns: new[] { "EmploymentTypeId", "Name" },
+                values: new object[] { 1, "Contract" });
+
+            migrationBuilder.InsertData(
+                table: "EmploymentTypes",
+                columns: new[] { "EmploymentTypeId", "Name" },
+                values: new object[] { 2, "Permanent" });
+
+            migrationBuilder.InsertData(
+                table: "EmploymentTypes",
+                columns: new[] { "EmploymentTypeId", "Name" },
+                values: new object[] { 3, "Director" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "EmploymentTypes");
+        }
+    }
+}
