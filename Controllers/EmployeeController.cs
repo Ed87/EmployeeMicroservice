403b286a42d3eba8f6bdeba0d9b6cd15a9934568
Ed Tshuma <edtshuma@outlook.com>
2020-12-01 17:54:05 +0200

    Added synchronous controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using EmployeeAPI.Model;
+using EmployeeAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _employeeService.GetEmployees();
+            return new OkObjectResult(employees);
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var employee = _employeeService.GetEmployeeById(id);
+            return new OkObjectResult(employee);
+        }
+
+        // POST: api/Employee
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee employee)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _employeeService.CreateEmployee(employee);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = employee.EmployeeId }, employee);
+            }
+        }
+
+        // PUT: api/Employee/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Employee employee)
+        {
+            if (employee != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _employeeService.UpdateEmployee(employee);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _employeeService.DeleteEmployee(id);
+            return new OkResult();
+        }
+    }
+}
